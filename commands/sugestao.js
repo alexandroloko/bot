5f3f18075be42f4d20e2330bf86cda763b05de1a
alexandroloko <39981927+alexandroloko@users.noreply.github.com>
2018-06-06 03:26:13 -0300

    Delete sugestao.js

--- commands/sugestao.js
+++
@@ -1,6 +0,0 @@
-exports.run = (client,message,args)=>{
-    var sugest = args.slice(0).join(" ")
-    if(!sugest) return message.reply("escreva sua sugestão")
-        client.guilds.get("307503384707989504").channels.get("452710273694957588").send("----------------------Sugestão------------------------\n"+ sugest + "\n \n sugestão enviada por <@" + message.author.id + "> ou " + message.author.tag + "\n \n---------------------fim--------------------")
-    message.channel.send("Mensagem enviada com sucesso!")
-}
